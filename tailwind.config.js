178949c8669314335b39d8f63554ebdf8da01fcf
nmogil <noah.mogil97@gmail.com>
2025-10-09 14:54:52 -0700

    Phase 2: Build FuzzyText Component #2

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,15 @@ export default {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)'
       },
-      colors: {}
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))'
+        },
+        border: 'hsl(var(--border))'
+      }
     }
   },
   plugins: [],
