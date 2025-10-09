08b3aa2c33637332317b497455833c3bcf4c3d36
nmogil <noah.mogil97@gmail.com>
2025-10-09 15:13:54 -0700

    Phase 3: Build Dither Background Component #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,41 @@
+import Dither from './components/Dither'
 import FuzzyText from './components/FuzzyText'
 
 function App() {
   return (
-    <div style={{
-      width: '100vw',
-      height: '100vh',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center'
-    }}>
-      <FuzzyText
-        baseIntensity={0.2}
-        hoverIntensity={0.5}
-        enableHover={true}
-      >
-        MOGIL VENTURES
-      </FuzzyText>
+    <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
+      <div style={{ position: 'absolute', inset: 0, zIndex: 0 }}>
+        <Dither
+          waveColor={[0.5, 0.5, 0.5]}
+          disableAnimation={false}
+          enableMouseInteraction={true}
+          mouseRadius={0.3}
+          colorNum={4}
+          waveAmplitude={0.3}
+          waveFrequency={3}
+          waveSpeed={0.05}
+        />
+      </div>
+      <div style={{
+        position: 'relative',
+        zIndex: 1,
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        pointerEvents: 'none'
+      }}>
+        <div style={{ pointerEvents: 'auto' }}>
+          <FuzzyText
+            baseIntensity={0.2}
+            hoverIntensity={0.5}
+            enableHover={true}
+          >
+            MOGIL VENTURES
+          </FuzzyText>
+        </div>
+      </div>
     </div>
   )
 }
